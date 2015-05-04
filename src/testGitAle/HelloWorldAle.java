2c2908c13acaa73bacc6ddf6d4240bbab09ba88a
valerio <valerio_cetorelli@yahoo.it>
2015-05-04 18:41:30 +0200

    prova valerio

--- src/testGitAle/HelloWorldAle.java
+++ src/testGitAle/HelloWorldAle.java
@@ -1,9 +1,9 @@
 package testGitAle;
 
 public class HelloWorldAle {
-	
+
 	public static void main (String args[])	{
 		System.out.println("Ciao Mondo, sono lusingato.. Brunooooooo");
+		System.out.println("Prova valerio");
 	}
-
 }
